4e187416ad46e6618d859d825493d7a96a3937e6
grosser <grosser.michael@gmail.com>
2011-10-19 21:47:00 +0200

    add unit test for list_definitions

--- spec/ruby-build_spec.rb
+++ spec/ruby-build_spec.rb
@@ -7,15 +7,26 @@ describe "ruby-build" do
     output
   end
 
-  it "shows help via --help" do
-    run("--help").should include('List all built-in definitions')
+  describe "commandline interface" do
+    it "shows help via --help" do
+      run("--help").should include('List all built-in definitions')
+    end
+
+    it "shows help via -h" do
+      run("-h").should include('List all built-in definitions')
+    end
+
+    it "shows version" do
+      run("--version").should =~ /^ruby-build \d+$/m
+    end
   end
 
-  it "shows help via -h" do
-    run("-h").should include('List all built-in definitions')
-  end
-
-  it "shows version" do
-    run("--version").should =~ /^ruby-build \d+$/m
+  describe "list_definitions" do
+    it "shows all ruby versions" do
+      results = run("--test list_definitions").split(' ')
+      results.size.should > 10
+      results.first.should == "1.8.6-p420"
+      results.last.should == "ree-1.8.7-2011.03"
+    end
   end
 end
