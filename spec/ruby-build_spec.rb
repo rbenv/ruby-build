bfee4c435a1dd89b26845fd4bb594fbfc0b68826
grosser <grosser.michael@gmail.com>
2011-10-19 21:36:58 +0200

    test basic api

--- spec/ruby-build_spec.rb
+++ spec/ruby-build_spec.rb
@@ -1,5 +1,21 @@
-describe "Foo" do
-  it "is bar" do
-    1.should == 1
+EXECUTABLE = File.expand_path('../../bin/ruby-build', __FILE__)
+
+describe "ruby-build" do
+  def run(cmd)
+    output = `#{EXECUTABLE} #{cmd} 2>&1`
+    raise "failed" unless $?.success?
+    output
+  end
+
+  it "shows help via --help" do
+    run("--help").should include('List all built-in definitions')
+  end
+
+  it "shows help via -h" do
+    run("-h").should include('List all built-in definitions')
+  end
+
+  it "shows version" do
+    run("--version").should =~ /^ruby-build \d+$/m
   end
 end
