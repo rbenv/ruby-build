779a2fa49fe178973041c74347c581cf0e9354e3
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-04-12 16:33:37 +0200

    Install libexec/*

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ fi
 
 BIN_PATH="${PREFIX}/bin"
 SHARE_PATH="${PREFIX}/share/ruby-build"
+LIBEXEC_PATH="${PREFIX}/libexec"
 
 mkdir -p "${BIN_PATH}"
 mkdir -p "${SHARE_PATH}"
+mkdir -p "${LIBEXEC_PATH}"
 
 for file in bin/*; do
   cp "${file}" "${BIN_PATH}"
@@ -18,6 +20,10 @@ done
 
 for file in share/ruby-build/*; do
   cp "${file}" "${SHARE_PATH}"
+done
+
+for file in libexec/*; do
+  cp "${file}" "${LIBEXEC_PATH}"
 done
 
 echo "Installed ruby-build at ${PREFIX}"
