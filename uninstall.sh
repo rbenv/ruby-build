b463583723d242af49a8ee7d5b90961e70724f18
Byron Gibson <fbg111@gmail.com>
2012-06-17 18:21:56 -0700

    fixed bash syntax errors

--- uninstall.sh
+++ uninstall.sh
@@ -9,19 +9,19 @@ fi
 BIN_PATH="${PREFIX}/bin"
 SHARE_PATH="${PREFIX}/share/ruby-build"
 
-if [ -d "${BIN_PATH}" ]
+if [ -d "${BIN_PATH}" ]; then
     for file in bin/*; do
         rm "${BIN_PATH}/${file}"
     done
-then
+else
     echo "${BIN_PATH} does not exist, are you sure you installed ruby-build standalone, and not as an rbenv plugin (rbenv/plugins/ruby-build)?"
 fi
 
-if [ -d "${SHARE_PATH}" ]
+if [ -d "${SHARE_PATH}" ]; then
     for file in share/ruby-build/*; do
         rm "${SHARE_PATH}/${file}" 
     done
-then
+else
     echo "${SHARE_PATH} does not exist, are you sure you installed ruby-build standalone, and not as an rbenv plugin (rbenv/plugins/ruby-build)?"
 fi
 
