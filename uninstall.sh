d2bb2ace8e8194585edb92869e7410d179cbc4ad
Byron Gibson <fbg111@gmail.com>
2012-06-17 18:06:18 -0700

    fixed error in uninstall.sh if statements, added *then* section

--- uninstall.sh
+++ uninstall.sh
@@ -13,12 +13,16 @@ if [ -d "${BIN_PATH}" ]
     for file in bin/*; do
         rm "${BIN_PATH}/${file}"
     done
+then
+    echo "${BIN_PATH} does not exist, are you sure you installed ruby-build standalone, and not as an rbenv plugin (rbenv/plugins/ruby-build)?"
 fi
 
 if [ -d "${SHARE_PATH}" ]
     for file in share/ruby-build/*; do
         rm "${SHARE_PATH}/${file}" 
     done
+then
+    echo "${SHARE_PATH} does not exist, are you sure you installed ruby-build standalone, and not as an rbenv plugin (rbenv/plugins/ruby-build)?"
 fi
 
 echo "Uninstalled ruby-build"
